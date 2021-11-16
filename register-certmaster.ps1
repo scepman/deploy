df7f29299e7d88d5697b08787b6f59d4d7fc6358
Christoph <christoph.hannebauer@glueckkanja-gab.com>
2021-11-16 14:33:56 +0100

    some more registering script

--- register-certmaster.ps1
+++ register-certmaster.ps1
@@ -1,6 +1,18 @@
 ﻿Connect-AzureAD
 
 $certMasterBaseURL = "https://as-certmaster-askjvljweklraesr.azurewebsites.net"
- 
-$reg = New-AzureADApplication -DisplayName "CertMaster v2.0" -ReplyUrls "$certMasterBaseURL/signin-oidc" -LogoutUrl "$certMasterBaseURL/signout-callback-oidc"
+
+$wellKnownGuidGraphApi = "00000003-0000-0000-c000-000000000000"
+
+# Add Directory.Read.All access
+$svcPrincipal = Get-AzureADServicePrincipal -All $true | ? { $_.AppId -eq $wellKnownGuidGraphApi }
+$appRole = $svcPrincipal.Oauth2Permissions | ? { $_.Value -eq "Directory.Read.All" }
+$appPermission = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList $appRole.Id, "Scope"
+
+$reqGraph = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
+$reqGraph.ResourceAppId = $wellKnownGuidGraphApi
+$reqGraph.ResourceAccess = $appPermission
+
+
+$application = New-AzureADApplication -DisplayName "CertMaster v2.0" -ReplyUrls "$certMasterBaseURL/signin-oidc" -LogoutUrl "$certMasterBaseURL/signout-callback-oidc" -RequiredResourceAccess $reqGraph
 
