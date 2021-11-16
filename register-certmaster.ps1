551415194c1cdb5655c582d25bf3fb9d5a8727d4
Christoph <christoph.hannebauer@glueckkanja-gab.com>
2021-11-16 14:33:32 +0100

    try out registering app

---
+++ register-certmaster.ps1
@@ -0,0 +1,6 @@
+﻿Connect-AzureAD
+
+$certMasterBaseURL = "https://as-certmaster-askjvljweklraesr.azurewebsites.net"
+ 
+$reg = New-AzureADApplication -DisplayName "CertMaster v2.0" -ReplyUrls "$certMasterBaseURL/signin-oidc" -LogoutUrl "$certMasterBaseURL/signout-callback-oidc"
+
